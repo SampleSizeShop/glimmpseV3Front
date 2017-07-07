168e0d9886295f1464e8d8b122a98fec0d2ddc5e
alasdair-macleod <alasdair.macleod@ufl.edu>
2017-07-07 16:36:09 -0400

    VER3-137 pagen now renders mathJax Component, but mathjax looks like it really really wants me to use the CDN

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import {MatrixService} from "./matrix-service";
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -8,6 +9,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [MatrixService]
     }).compileComponents();
   }));
 
@@ -20,13 +22,19 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toContain('app');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome app');
+  }));
+
+  it('should should attach a message from service to the component', async(() => {
+    let fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    expect(fixture.componentInstance.message).toBe('real service');
   }));
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {MatrixService} from './matrix-service';
+import {MatrixComponent} from './matrix.component';
+import {MathJaxDirective} from './mathjax.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatrixComponent,
+    MathJaxDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [MatrixService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
