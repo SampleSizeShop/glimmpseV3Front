3d457b1b04b3400798bf9decb15e9357bbf7c1bd
alasdair-macleod <alasdair.macleod@ufl.edu>
2017-07-10 14:50:22 -0400

    VER3-137 Fix test spec

--- karma.conf.js
+++ karma.conf.js
@@ -28,15 +28,6 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
-    customLaunchers: {
-      Chrome_travis_ci: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    }
+    singleRun: false
   });
 };
-if(process.env.TRAVIS) {
-  configuration.browsers = ['Chrome_travis_ci'];
-}
