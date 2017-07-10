2b94ad3e2028905a12092cbf79201d6c9f9ffeab
alasdair-macleod <alasdair.macleod@ufl.edu>
2017-07-10 12:46:03 -0400

    VER3-137 Fixed Tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,17 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
-import {MatrixService} from "./matrix-service";
+import {MatrixService} from './matrix-service';
+import { MatrixComponent } from './matrix.component';
+import {FormsModule} from '@angular/forms';
+import {MathJaxDirective} from './mathjax.directive';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ FormsModule ],
       declarations: [
-        AppComponent
+        AppComponent, MatrixComponent, MathJaxDirective
       ],
       providers: [MatrixService]
     }).compileComponents();
