c792d64412d2c2ff19d719aa1feb3769b83d5a4b
alasdair-macleod <alasdair.macleod@ufl.edu>
2017-07-11 09:25:39 -0400

    VER3-137 remove mathJax and add tests back top build

--- src/app/flask.directive.ts
+++ src/app/flask.directive.ts
@@ -1,7 +1,7 @@
 import {Directive, ElementRef, Input, OnChanges, OnInit} from "@angular/core";
 
 @Directive({
-  selector: '[mathJaxExpr]'})
+  selector: '[flaskExpr]'})
 export class FlaskDirective implements OnInit, OnChanges {
   @Input("flaskExpr") private value: string = "";
   constructor(private element: ElementRef){
