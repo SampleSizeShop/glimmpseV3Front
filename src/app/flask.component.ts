9a4909dfd85d8720d3bf1ceab2d3ffb3f4de8a2f
alasdair-macleod <alasdair.macleod@ufl.edu>
2017-07-11 10:27:56 -0400

    VER3-137 re add mathJax via CND

--- src/app/flask.component.ts
+++ src/app/flask.component.ts
@@ -6,5 +6,5 @@ import {Component} from '@angular/core';
   templateUrl: './flask.component.html'
 })
 export class FlaskComponent {
-  output: string = "$$M = \\begin{bmatrix}x_1 & \\cdots & n_1 \\\\x_2 & \\cdots & n_2 \\\\x_3 & \\cdots & n_3 \\\\x_4 & \\cdots & n_4 \\\\x_5 & \\cdots & n_5 \\end{bmatrix}$$";
+  output: string = "$M = \\begin{bmatrix}x_1 & \\cdots & n_1 \\\\x_2 & \\cdots & n_2 \\\\x_3 & \\cdots & n_3 \\\\x_4 & \\cdots & n_4 \\\\x_5 & \\cdots & n_5 \\end{bmatrix}$";
 }
