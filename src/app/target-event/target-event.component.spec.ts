bc8d87a16cb8e360f7c804007f821a25a63be93a
alasdair-macleod <alasdair.macleod@ufl.edu>
2017-08-02 16:35:06 -0400

    VER3-162 re organise target event component testing for clarity

--- src/app/target-event/target-event.component.spec.ts
+++ src/app/target-event/target-event.component.spec.ts
@@ -8,7 +8,7 @@ import {Http} from '@angular/http';
 import {DebugElement} from '@angular/core';
 import {By} from '@angular/platform-browser';
 
-describe('TargetEventComponent', () => {
+describe('TargetEventComponent_targetEvent_REJECTION', () => {
   let component: TargetEventComponent;
   let fixture: ComponentFixture<TargetEventComponent>;
 
@@ -21,52 +21,66 @@ describe('TargetEventComponent', () => {
     .compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TargetEventComponent);
+    component = fixture.componentInstance;
+    component.selectRejectionOnly();
+    fixture.detectChanges();
+  });
+
   it('should be created with rejection selected', () => {
     expect(component).toBeTruthy();
     component.isRejection()
   });
 
-  it('select RejectionOnly should ste targetEvent to rejection', () => {
-    component.selectRejectionOnly();
-    expect(component.isRejection())
-  });
-
-  it('select CI Width should ste targetEvent to CI width', () => {
-    component.selectCIWidth();
-    expect(component.isCIWidth())
-  });
-
-  it('select WAVR should ste targetEvent to WAVR', () => {
-    component.selectCIWidth();
-    expect(component.isCIWidth())
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TargetEventComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('select RejectionOnly should set targetEvent to rejection', () => {
   });
 
   it('should show rejection only description when rejection is selected', () => {
-    component.selectRejectionOnly();
-    fixture.detectChanges();
     const desc: DebugElement = fixture.debugElement.query(By.css('#rejectiondesc'));
     const el: HTMLElement = desc.nativeElement;
     expect(el);
   });
 
   it('should not show CIWidth description when rejection is selected', () => {
-    component.selectRejectionOnly();
-    fixture.detectChanges();
     const desc: DebugElement = fixture.debugElement.query(By.css('#ciwidthdesc'));
     expect(!desc);
   });
 
   it('should not show WAVR description when rejection is selected', () => {
-    component.selectRejectionOnly();
-    fixture.detectChanges();
     const desc: DebugElement = fixture.debugElement.query(By.css('#wavrdesc'));
     expect(!desc);
+  });
+
+  it('should give Rejection button active class if Rejection Mode is selected', () => {
+    const desc: DebugElement = fixture.debugElement.query(By.css('.active'));
+    const el = desc.nativeElement;
+    expect(el.id).toEqual('rejectionbtn')
+  });
+});
+
+describe('TargetEventComponent_targetEvent_CIWIDTH', () => {
+  let component: TargetEventComponent;
+  let fixture: ComponentFixture<TargetEventComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
+      declarations: [TargetEventComponent],
+      providers: [StudyService, {provide: Http, useClass: MockBackend}]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TargetEventComponent);
+    component = fixture.componentInstance;
+    component.selectCIWidth();
+    fixture.detectChanges();
+  });
+
+  it('select CI Width should set targetEvent to CI width', () => {
+    expect(component.isCIWidth())
   });
 
   it('should show ciwidth description when ci width is selected', () => {
@@ -77,18 +91,47 @@ describe('TargetEventComponent', () => {
     expect(el);
   });
 
-  it('should not show Rejection description when rejection is selected', () => {
-    component.selectCIWidth();
-    fixture.detectChanges();
+  it('should not show Rejection description when ci width is selected', () => {
     const desc: DebugElement = fixture.debugElement.query(By.css('#rejectiondesc'));
     expect(!desc);
   });
 
   it('should not show WAVR description when ci width is selected', () => {
-    component.selectCIWidth();
-    fixture.detectChanges();
     const desc: DebugElement = fixture.debugElement.query(By.css('#wavrdesc'));
     expect(!desc);
+  });
+
+  it('should give CI width button active class if CI Width Mode is selected', () => {
+    component.selectCIWidth();
+    fixture.detectChanges();
+    const desc: DebugElement = fixture.debugElement.query(By.css('.active'));
+    const el = desc.nativeElement;
+    expect(el.id).toEqual('ciwidthbtn')
+  });
+});
+
+describe('TargetEventComponent_targetEvent_WAVR', () => {
+  let component: TargetEventComponent;
+  let fixture: ComponentFixture<TargetEventComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
+      declarations: [TargetEventComponent],
+      providers: [StudyService, {provide: Http, useClass: MockBackend}]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TargetEventComponent);
+    component = fixture.componentInstance;
+    component.selectWAVR();
+    fixture.detectChanges();
+  });
+
+  it('select WAVR should set targetEvent to WAVR', () => {
+    expect(component.isWAVR())
   });
 
   it('should show WAVR description when WAVR is selected', () => {
@@ -100,33 +143,13 @@ describe('TargetEventComponent', () => {
   });
 
   it('should not show Rejection description when WAVR is selected', () => {
-    component.selectWAVR();
-    fixture.detectChanges();
     const desc: DebugElement = fixture.debugElement.query(By.css('#rejectiondesc'));
     expect(!desc);
   });
 
   it('should not show CI width description when WAVR is selected', () => {
-    component.selectWAVR();
-    fixture.detectChanges();
     const desc: DebugElement = fixture.debugElement.query(By.css('#ciwidthdesc'));
     expect(!desc);
-  });
-
-  it('should give Rejection button active class if Rejection Mode is selected', () => {
-    component.selectRejectionOnly();
-    fixture.detectChanges();
-    const desc: DebugElement = fixture.debugElement.query(By.css('.active'));
-    const el = desc.nativeElement;
-    expect(el.id).toEqual('rejectionbtn')
-  });
-
-  it('should give CI width button active class if CI Width Mode is selected', () => {
-    component.selectCIWidth();
-    fixture.detectChanges();
-    const desc: DebugElement = fixture.debugElement.query(By.css('.active'));
-    const el = desc.nativeElement;
-    expect(el.id).toEqual('ciwidthbtn')
   });
 
   it('should give WAVR button active class if WAVR is selected', () => {
