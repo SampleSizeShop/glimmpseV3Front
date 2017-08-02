e2bbd4c680761df345dd7b88814dafcf0603276b
alasdair-macleod <alasdair.macleod@ufl.edu>
2017-08-02 15:29:32 -0400

    VER3-162 tidy up imports

--- src/app/study-form/study-form.component.spec.ts
+++ src/app/study-form/study-form.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { StudyFormComponent } from './study-form.component';
+import {StudyFormComponent} from './study-form.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {StudyService} from '../shared/study.service';
 import {UserModeComponent} from '../user-mode/user-mode.component';
@@ -9,7 +9,6 @@ import {SolveForComponent} from '../solve-for/solve-for.component';
 import {MockBackend} from '@angular/http/testing';
 import {Http} from '@angular/http';
 import {LoggerModule, NGXLogger, NGXLoggerMock} from 'ngx-logger';
-import any = jasmine.any;
 
 describe('StudyFormComponent', () => {
   let component: StudyFormComponent;
