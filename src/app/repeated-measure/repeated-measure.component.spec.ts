d87d5776373f818c1be608ec0fbeef6d9782ae90
alasdair-macleod <alasdair.macleod@ufl.edu>
2017-08-15 13:52:22 -0400

    VER3-176 repeated measure component tested

--- src/app/repeated-measure/repeated-measure.component.spec.ts
+++ src/app/repeated-measure/repeated-measure.component.spec.ts
@@ -7,6 +7,8 @@ import {RepeatedMeasureService} from '../shared/repeatedMeasure.service';
 import {RepeatedMeasure} from '../shared/RepeatedMeasure';
 import {Http} from '@angular/http';
 import {MockBackend} from '@angular/http/testing';
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
 
 describe('RepeatedMeasureComponent', () => {
   let component: RepeatedMeasureComponent;
@@ -29,5 +31,39 @@ describe('RepeatedMeasureComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  const validFormSetup = function () {
+    component.repeatedMeasureForm.controls['name'].setValue('2');
+    component.repeatedMeasureForm.controls['noRepeats'].setValue(2);
+    component.repeatedMeasureForm.controls['spacing'].setValue(2);
+  };
+
+  it('Should be valid when required input is set', () => {
+    validFormSetup();
+    expect(component.repeatedMeasureForm.valid).toBeTruthy();
+  });
+
+  it('Should show correlation matrix component when form is valid', () => {
+    validFormSetup();
+    const desc: DebugElement = fixture.debugElement.query(By.css('#correlationMatrix'));
+    expect(desc);
+  });
+
+  it('Should not show correlation matrix component when form is not valid', () => {
+    const desc: DebugElement = fixture.debugElement.query(By.css('#correlationMatrix'));
+    expect(!desc);
+  });
+
+  it('Should have add measure button enabled when form is valid', () => {
+    validFormSetup();
+    fixture.detectChanges();
+    const btn: DebugElement = fixture.debugElement.query(By.css('#addMeasureBtn'));
+    expect(btn.nativeElement.disabled).toBeFalsy();
+  });
+
+  it('Should not have add measure button enabled when form is not valid', () => {
+    const btn: DebugElement = fixture.debugElement.query(By.css('#addMeasureBtn'));
+    expect(btn.nativeElement.disabled).toBeTruthy();
   });
 });
