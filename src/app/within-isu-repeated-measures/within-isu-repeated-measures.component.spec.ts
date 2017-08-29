19bc2de96ffe5c7552a2783c743f98462337f3b2
alasdair-macleod <alasdair.macleod@ufl.edu>
2017-08-29 10:59:16 -0400

    VER3-218 fix tests.

--- src/app/within-isu-repeated-measures/within-isu-repeated-measures.component.spec.ts
+++ src/app/within-isu-repeated-measures/within-isu-repeated-measures.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { WithinIsuRepeatedMeasuresComponent } from './within-isu-repeated-measures.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {StudyService} from '../shared/study.service';
+import {Http} from '@angular/http';
+import {MockBackend} from '@angular/http/testing';
+import {NavigationService} from '../shared/navigation.service';
 
 describe('WithinIsuRepeatedMeasuresComponent', () => {
   let component: WithinIsuRepeatedMeasuresComponent;
@@ -8,7 +13,9 @@ describe('WithinIsuRepeatedMeasuresComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ WithinIsuRepeatedMeasuresComponent ]
+      imports: [ReactiveFormsModule],
+      declarations: [ WithinIsuRepeatedMeasuresComponent ],
+      providers: [StudyService, {provide: Http, useClass: MockBackend}, NavigationService]
     })
     .compileComponents();
   }));
