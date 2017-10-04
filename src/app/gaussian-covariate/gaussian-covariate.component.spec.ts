04fd7b97b73ac182e3835f0fd3a245c9eb82cf2e
alasdair-macleod <alasdair.macleod@ufl.edu>
2017-10-04 14:34:31 -0400

    VER3-228 add variance form to gaussian covariate component

--- src/app/gaussian-covariate/gaussian-covariate.component.spec.ts
+++ src/app/gaussian-covariate/gaussian-covariate.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { GaussianCovariateComponent } from './gaussian-covariate.component';
 import {DebugElement} from '@angular/core';
 import {By} from '@angular/platform-browser';
+import {ReactiveFormsModule} from '@angular/forms';
 
 describe('GaussianCovariateComponent', () => {
   let component: GaussianCovariateComponent;
@@ -10,6 +11,7 @@ describe('GaussianCovariateComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
       declarations: [ GaussianCovariateComponent ]
     })
     .compileComponents();
@@ -28,6 +30,15 @@ describe('GaussianCovariateComponent', () => {
   it( 'should show ask the user if they want to define a gaussian covatiate,' +
     'if there is not one added and they are not currently editing one.', () => {
     const desc: DebugElement = fixture.debugElement.query(By.css('#firstgaussiancovariate'));
+    const el = desc.nativeElement;
+    expect(el).toBeTruthy();
+  });
+
+  it( 'should ask the user to define a variance for their gaussian covariate when the click the' +
+    'define a gaussian covariate button.', () => {
+    component.includeGaussianCovariate();
+    fixture.detectChanges();
+    const desc: DebugElement = fixture.debugElement.query(By.css('#variance'));
     const el = desc.nativeElement;
     expect(el).toBeTruthy();
   });
--- src/app/gaussian-covariate/gaussian-covariate.component.ts
+++ src/app/gaussian-covariate/gaussian-covariate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-gaussian-covariate',
@@ -6,16 +7,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gaussian-covariate.component.css']
 })
 export class GaussianCovariateComponent implements OnInit {
+  private _gaussianCovariateForm: FormGroup;
+  private _editing: boolean;
 
-  constructor() { }
+  constructor(private _fb: FormBuilder) {
+    this.editing = false;
+  }
 
   ngOnInit() {
+    this.buildForm()
   }
 
-  includeGaussianCovariate() {}
+  buildForm() {
+    this.gaussianCovariateForm = this.fb.group({
+      variance: [0]
+    });
+  }
+
+  includeGaussianCovariate() {
+    this.editing = true;
+  }
 
   hasGaussianCovariate() {
-    return false;
+    return this.editing;
   }
 
+  get gaussianCovariateForm(): FormGroup {
+    return this._gaussianCovariateForm;
+  }
+
+  set gaussianCovariateForm(value: FormGroup) {
+    this._gaussianCovariateForm = value;
+  }
+
+  get editing(): boolean {
+    return this._editing;
+  }
+
+  set editing(value: boolean) {
+    this._editing = value;
+  }
+
+  get fb(): FormBuilder {
+    return this._fb;
+  }
+
+  set fb(value: FormBuilder) {
+    this._fb = value;
+  }
 }
