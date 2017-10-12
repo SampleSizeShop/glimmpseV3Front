509270a50687e05064e6aa3c24a8b3b5391fb12e
alasdair-macleod <alasdair.macleod@ufl.edu>
2017-10-12 10:40:00 -0400

    VER3-229 selecting hypothesis types

--- src/app/hypothesis-between/hypothesis-between.component.ts
+++ src/app/hypothesis-between/hypothesis-between.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {constants} from 'app/shared/constants';
 
 @Component({
   selector: 'app-hypothesis-between',
@@ -7,12 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HypothesisBetweenComponent implements OnInit {
   private _showAdvancedOptions: boolean;
+  private _betweenHypothesisType: string;
+  private _HYPOTHESIS_TYPE = constants.HYPOTHESIS_TYPE;
 
   constructor() {
     this.showAdvancedOptions = false;
   }
 
   ngOnInit() {
+    this.betweenHypothesisType = constants.HYPOTHESIS_TYPE.GLOBAL_TRENDS;
+  }
+
+  isSelected(hypothesis: string): boolean {
+    return this.betweenHypothesisType === hypothesis;
+  }
+
+  selectHypothesisType(type: string) {
+    this.betweenHypothesisType = type;
   }
 
   toggleAdvancedOptions() {
@@ -25,5 +37,21 @@ export class HypothesisBetweenComponent implements OnInit {
 
   set showAdvancedOptions(value: boolean) {
     this._showAdvancedOptions = value;
+  }
+
+  get betweenHypothesisType(): string {
+    return this._betweenHypothesisType;
+  }
+
+  set betweenHypothesisType(value: string) {
+    this._betweenHypothesisType = value;
+  }
+
+  get HYPOTHESIS_TYPE() {
+    return this._HYPOTHESIS_TYPE;
+  }
+
+  set HYPOTHESIS_TYPE(value) {
+    this._HYPOTHESIS_TYPE = value;
   }
 }
--- src/app/hypothesis-within/hypothesis-within.component.ts
+++ src/app/hypothesis-within/hypothesis-within.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {constants} from '../shared/constants';
 
 @Component({
   selector: 'app-hypothesis-within',
@@ -7,12 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HypothesisWithinComponent implements OnInit {
   private _showAdvancedOptions: boolean;
+  private _withinHypothesisType: string;
+  private _HYPOTHESIS_TYPE = constants.HYPOTHESIS_TYPE;
 
   constructor() {
     this.showAdvancedOptions = false;
   }
 
   ngOnInit() {
+    this.withinHypothesisType = constants.HYPOTHESIS_TYPE.GLOBAL_TRENDS;
+  }
+
+  isSelected(hypothesis: string): boolean {
+    return this.withinHypothesisType === hypothesis;
+  }
+
+  selectHypothesisType(type: string) {
+    this.withinHypothesisType = type;
   }
 
   toggleAdvancedOptions() {
@@ -25,5 +37,21 @@ export class HypothesisWithinComponent implements OnInit {
 
   set showAdvancedOptions(value: boolean) {
     this._showAdvancedOptions = value;
+  }
+
+  get withinHypothesisType(): string {
+    return this._withinHypothesisType;
+  }
+
+  set withinHypothesisType(value: string) {
+    this._withinHypothesisType = value;
+  }
+
+  get HYPOTHESIS_TYPE(): { GLOBAL_TRENDS; ALL_PAIRWISE; SUCCESSIVE_PAIRS; IDENTITY; POLYNOMIAL; USER_DEFINED } | any {
+    return this._HYPOTHESIS_TYPE;
+  }
+
+  set HYPOTHESIS_TYPE(value: { GLOBAL_TRENDS; ALL_PAIRWISE; SUCCESSIVE_PAIRS; IDENTITY; POLYNOMIAL; USER_DEFINED } | any) {
+    this._HYPOTHESIS_TYPE = value;
   }
 }
