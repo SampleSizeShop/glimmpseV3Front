4b325bbc25ce62ac95be07c3f91eee9da70897a2
alasdair.macleod <alasdair.macleod@ufl.edu>
2017-11-17 13:22:19 -0500

    VER3-236 displaying outcome vs repeated measure st devs

--- src/app/parameters-repeated-measure-outcome-correlations/parameters-repeated-measure-outcome-correlations.component.ts
+++ src/app/parameters-repeated-measure-outcome-correlations/parameters-repeated-measure-outcome-correlations.component.ts
@@ -1,5 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ISUFactors} from '../shared/ISUFactors';
+import {ISUFactor} from '../shared/ISUFactor';
+import {Outcome} from "../shared/Outcome";
+import {RepeatedMeasure} from "../shared/RepeatedMeasure";
+import {ISUFactorCombination} from "../shared/ISUFactorCombination";
 
 @Component({
   selector: 'app-parameters-repeated-measure-outcome-correlations',
@@ -8,9 +12,22 @@ import {ISUFactors} from '../shared/ISUFactors';
 })
 export class ParametersRepeatedMeasureOutcomeCorrelationsComponent implements OnInit {
   private _isuFactors: ISUFactors;
+  private _combinations: Map<ISUFactor, Array<ISUFactor>>;
+  private map: Map<string, string[]>;
   constructor() { }
 
   ngOnInit() {
+    this.setMap();
+  }
+
+  setMap() {
+    this.map = new Map<string, string[]>();
+    this.isuFactors.outcomes.forEach(outcome => {
+      this.isuFactors.repeatedMeasures.forEach(measure => {
+        const vals = new Array<string>(measure.valueNames.length).fill('1');
+        this.map.set(outcome.name + ':' + measure.name, vals);
+      })
+    })
   }
 
   get isuFactors(): ISUFactors {
@@ -19,5 +36,13 @@ export class ParametersRepeatedMeasureOutcomeCorrelationsComponent implements On
 
   @Input() set isuFactors(value: ISUFactors) {
     this._isuFactors = value;
+  }
+
+  get combinations(): Map<ISUFactor, Array<ISUFactor>> {
+    return this._combinations;
+  }
+
+  set combinations(value: Map<ISUFactor, Array<ISUFactor>>) {
+    this._combinations = value;
   }
 }
