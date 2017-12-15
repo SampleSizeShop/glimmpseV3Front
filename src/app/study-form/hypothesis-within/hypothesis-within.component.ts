cac45941ba615ed0969b09512c558219a7553ff2
alasdair.macleod <alasdair.macleod@ufl.edu>
2017-12-15 12:32:12 -0500

    VER3-236 fix kroneker for uMatrix

--- src/app/study-form/hypothesis-within/hypothesis-within.component.ts
+++ src/app/study-form/hypothesis-within/hypothesis-within.component.ts
@@ -152,8 +152,8 @@ export class HypothesisWithinComponent implements OnInit, OnDestroy {
   }
 
   get uMatrix() {
-    let m = this.uOutcomes.multiply(this.uCluster);
-    m = math.kron(m, this.uRepeatedMeasures.values);
+    let m = this.uOutcomes.kronecker(this.uRepeatedMeasures);
+    m = math.kron(m, math.matrix([this.uCluster]));
     let texString = '$\\begin{bmatrix}';
     let row = 0;
     m.forEach(function (value, index, matrix) {
