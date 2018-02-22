bebb47a7406c820f2fd2222ebcd4419869375e0b
alasdair-macleod <alasdair.macleod@ufl.edu>
2018-02-22 16:04:29 -0500

    VER3-282 update javadoc on Cluster and ClusterGuard component

--- src/app/shared/cluster-guard.service.ts
+++ src/app/shared/cluster-guard.service.ts
@@ -6,17 +6,34 @@ import {Subscription} from 'rxjs/Subscription';
 import {isNullOrUndefined} from 'util';
 import {NGXLogger} from 'ngx-logger';
 
+/**
+ * ClusterGuard guard allows access to a route if and only if the currently loaded StudyDesign has clustering defined.
+ */
 @Injectable()
 export class ClusterGuard implements CanActivate {
   private isuFactors: ISUFactors;
   private isuFactorsSubscription: Subscription;
 
+  /**
+   * Default constructor.
+   *
+   * @param {Router} router
+   * @param {StudyService} study_service
+   * @param {NGXLogger} log
+   */
   constructor(private router: Router, private study_service: StudyService, private log: NGXLogger) {
     this.isuFactorsSubscription = this.study_service.isuFactors$.subscribe( isuFactors => {
       this.isuFactors = isuFactors;
     } );
   }
 
+  /**
+   * Allows access to a route if and only if the currently loaded StudyDesign has clustering defined.
+   *
+   * @param {ActivatedRouteSnapshot} route
+   * @param {RouterStateSnapshot} state
+   * @returns {boolean}
+   */
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     this.log.debug('ClusterGuard#canActivate called');
     const st = this.study_service.stage;
