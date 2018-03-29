c56fe257aaa9517570f6f71092071e94393af249
alasdair-macleod <alasdair.macleod@ufl.edu>
2018-03-29 15:41:37 -0400

    VER3-252 try to run fewer jobs

--- scripts/docker_deploy.sh
+++ scripts/docker_deploy.sh
@@ -6,25 +6,16 @@ echo REPO > sshenv
 echo IMAGE >> sshenv
 scp -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i ${EC2_SSH_KEY} sshenv ${EC2_URL}:~/
 ssh -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -i ${EC2_SSH_KEY} ${EC2_URL} /bin/bash << 'EOT'
-# #############################################################################################################
-# start marking variables for export
-# #############################################################################################################
+echo start marking variables for export
 set -a
 . ./sshenv
 set +a
-# #############################################################################################################
-# stop old container
-# #############################################################################################################
+echo stop old container
 sudo docker stop $(docker ps -q --filter ancestor=$REPO/$IMAGE:latest)
-# #############################################################################################################
-# log into EC2 instance. Pull and deploy new container.
-# ####################################ng build --prod
-#########################################################################
+echo log into EC2 instance. Pull and deploy new container.
 $(aws ecr get-login --no-include-email --region us-east-1)
 sudo docker pull $REPO/$IMAGE
 sudo docker run -d -p 80:80 $REPO/$IMAGE
-# #############################################################################################################
-# clean up
-# #############################################################################################################
+echo clean up
 rm -f sshenv
 EOT
