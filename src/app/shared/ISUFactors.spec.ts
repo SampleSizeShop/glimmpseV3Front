a57467f4dfa97362bc8bd4ac87cd3a3a0ac30aea
alasdair-macleod <alasdair.macleod@ufl.edu>
2018-04-03 16:29:32 -0400

    VER3-346 VER3-322 update tests for marginal means to JSON output

--- src/app/shared/ISUFactors.spec.ts
+++ src/app/shared/ISUFactors.spec.ts
@@ -13,9 +13,12 @@ describe('ISUFactors', () => {
   it( 'should correcly produce an object representation of marginal means from a Map', () => {
     const val0 = '0';
     const val1 = '1';
-    const comb0 = new ISUFactorCombination([new CombinationId('Time', 0)], 1);
-    const comb1 = new ISUFactorCombination([new CombinationId('Time', 1)], 1)
-    const expected = [{name: val0, ISUFactorCombination: comb0}, {name: val1, ISUFactorCombination: comb1}]
+    const comb0 = new ISUFactorCombination([new CombinationId('Time', val0)], 1);
+    const comb1 = new ISUFactorCombination([new CombinationId('Time', val1)], 1)
+    const expected = [
+      {name: val0, ISUFactorCombination: comb0},
+      {name: val1, ISUFactorCombination: comb1}
+      ];
     component.marginalMeans.set(val0, comb0);
     component.marginalMeans.set(val1, comb1);
     const actual = component.marginalMeansToArray();
