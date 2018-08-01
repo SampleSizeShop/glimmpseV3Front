a779698858b9fc7ed67e47cc4e1eded24aa5d8cb
alasdair-macleod <alasdair.macleod@ufl.edu>
2018-08-01 17:12:13 -0400

    VER3-361 added calculate button and primitive results display

--- src/app/study-form/calculate/calculate.component.ts
+++ src/app/study-form/calculate/calculate.component.ts
@@ -3,6 +3,7 @@ import {StudyDesign} from '../../shared/study-design';
 import {isNullOrUndefined} from 'util';
 import {StudyService} from '../study.service';
 import {Subscription} from 'rxjs/Subscription';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-calculate',
@@ -13,8 +14,9 @@ export class CalculateComponent implements OnInit {
   private _studyDesign: StudyDesign;
   private _studySubscription: Subscription;
   private _outputString: string;
+  private _resultString: string;
 
-  constructor(private study_service: StudyService) {
+  constructor(private study_service: StudyService, private http: HttpClient) {
     this.studySubscription = this.study_service.studyDesign$.subscribe( study => {
       this._studyDesign = study;
     });
@@ -24,8 +26,27 @@ export class CalculateComponent implements OnInit {
     if (!isNullOrUndefined(this._studyDesign)) {
       this.outputString = JSON.stringify(this._studyDesign);
     } else {
-      this.outputString = 'HMMM......'
+      this.outputString = 'HMMM......';
+      this.resultString = 'no results yet';
     }
+  }
+
+  postModel() {
+    const output = this.outputString;
+    this.http.post(
+      'http://127.0.0.1:5000/api/calculate',
+      output,
+      this.jsonHeader()).toPromise().then(response => this.resultString = response as string).catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
+  private jsonHeader() {
+    const header = {headers: new HttpHeaders({ 'Content-Type': 'application/json' })};
+    return header;
   }
 
   get outputString(): string {
@@ -42,5 +63,13 @@ export class CalculateComponent implements OnInit {
 
   set studySubscription(value: Subscription) {
     this._studySubscription = value;
+  }
+
+  get resultString(): string {
+    return this._resultString;
+  }
+
+  set resultString(value: string) {
+    this._resultString = value;
   }
 }
