fb3f6e07bb6abbb2d8d8307d92949189e5ef4ae6
alasdair-macleod <alasdair.macleod@ufl.edu>
2018-08-13 18:24:51 -0400

    VER3-365 simplify results page object

--- src/app/study-form/calculate/calculate.po.ts
+++ src/app/study-form/calculate/calculate.po.ts
@@ -4,18 +4,14 @@ export class CalculatePo {
 
   readOutput(): Promise<string> {
     return new Promise( (resolve, reject) => {
-      let invisible = true;
-      while (invisible) {
-        invisible = false;
-        if (element(by.id('results')).isDisplayed()) {
-          element(by.id('results')).getText().then(text => {
-            const str = text;
-            resolve(str);
-          });
-        } else {
-          console.log('nope');
-          invisible = true;
-        }
+      if (element(by.id('results')).isDisplayed()) {
+        element(by.id('results')).getText().then(text => {
+          const str = text;
+          resolve(str);
+        });
+      } else {
+        console.log('nope');
+        reject(false);
       }
     });
   }
