d0a6755fd7cd3c6bd032734e587dca7162600353
alasdair-macleod <alasdair.macleod@ufl.edu>
2018-08-16 09:37:12 -0400

    VER3-365 add test for Qians Example woith realistic factors

--- e2e/study-form.e2e-spec.ts
+++ e2e/study-form.e2e-spec.ts
@@ -1,8 +1,9 @@
 import {StudyFormComponentPage} from '../src/app/study-form/study-form.po';
 import {example_1_input, example_1_output} from './test_inputs/example_1_input';
 import {generic_model} from './test_inputs/generic_test_data_model';
+import {Qian_Ex_1, Qian_Ex_1_input, Qian_Ex_1_output} from './test_inputs/Qian_Ex_1';
 
-describe('demo-front-app short course homework test', () => {
+describe('Glimmpse v3 automated integration tests', () => {
   let page: StudyFormComponentPage;
 
   beforeEach(() => {
@@ -12,6 +13,19 @@ describe('demo-front-app short course homework test', () => {
   });
 
   it('Should fill out the study form', async function() {
+    const expected = Qian_Ex_1_output;
+    let actual = null;
+    await page.fromJSON(Qian_Ex_1_input);
+    await page.calculate();
+    await page.output().then(text => {
+      console.log(text);
+      actual = JSON.parse(text);
+    });
+    expect(actual.model).toEqual(expected.model);
+    expect(actual.results[0].power).toEqual(expected.results[0].power);
+  });
+
+  it('Should calculate a power of 1 for Grand Mean with only one outcome', async function() {
     const expected = example_1_output;
     let actual = null;
     await page.fromJSON(example_1_input);
@@ -24,7 +38,7 @@ describe('demo-front-app short course homework test', () => {
     expect(actual.results[0].power).toEqual(expected.results[0].power);
   });
 
-  it('Should fill out the study form', () => {
+  it('Should fill out the study form: Nonsense example designed to hit every page in glimmpse v3 front end', () => {
     page.fromJSON(generic_model);
   });
 });
---
+++ e2e/test_inputs/Qian_Ex_1.ts
@@ -0,0 +1,105 @@
+import {constants} from '../../src/app/shared/constants';
+
+export const Qian_Ex_1_output = {
+  message: "OK",
+  status: 200,
+  mimetype: "application/json",
+  results: [
+    {
+      test: "Hotelling Lawley Trace",
+      power: 1.0
+    }],
+  model: {
+    essence_design_matrix: [ [ 1 ] ],
+    repeated_rows_in_design_matrix: 10,
+    hypothesis_beta: [ [ 80 ] ],
+    c_matrix: [ [ 1 ] ],
+    u_matrix: [ [ 1 ] ],
+    sigma_star: [ [ 225 ] ],
+    theta_zero: [ [ 0 ] ],
+    alpha: 0.05,
+    total_n: 10,
+    theta: [ [ 80 ] ],
+    m: [ [ 1 ] ],
+    nu_e: 9,
+    hypothesis_sum_square: [ [ 64000 ] ],
+    error_sum_square: [ [ 2025 ] ]
+  }
+}
+
+export const Qian_Ex_1_input = {
+  user_mode: constants.USER_MODE.GUIDED,
+  target_event: constants.TARGET_EVENT.WAVR,
+  solve_for: null,
+  statistical_tests: [
+    constants.STATISTICAL_TESTS.HOTELLING_LAWLEY
+  ],
+  type_one_error: 0.05,
+  outcomes: ['hr', 'Vo'],
+  repeated_measures: [
+    {dimension: 'Time', units: 'days', type: constants.REPEATED_MEASURE_TYPES[0], values: [0, 1, 2, 3]},
+    {dimension: 'Device', type: constants.REPEATED_MEASURE_TYPES[1], values: [0, 1]}
+  ],
+  cluster: {element: 'Class', levels: [{name: 'level1', no_elements: 10}]},
+  predictors: [
+    {name: 'gender', groups: ['m', 'f']},
+    {name: 'race', groups: ['b', 'w', 'h']}],
+  smallest_group: 5,
+  groups: [
+    {group: '', table: [[1, 1, 1], [1, 1, 1]]}
+  ],
+  gaussian_covariate: null,
+  hypothesis: 'Time x Device x gender x race',
+  hypothesis_between: null,
+  hypothesis_within: null,
+  theta0: null,
+  marginal_means: [
+    {means: [
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1]]},
+    {means: [
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1]]},
+    ],
+  parameters_scale_factor: null,
+  parameters_standard_deviation: [
+    {outcome: 'hr', st_dev: 80},
+    {outcome: 'Vo', st_dev: 0.06}
+  ],
+  parameters_outcome_correlation: [
+    [null, null],
+    [0, null]
+  ],
+  parameters_outcome_repeated_measure_stdev: [
+    {stdevs: [1, 1, 1, 1]},
+    {stdevs: [1, 1]},
+    {stdevs: [1, 1, 1, 1]},
+    {stdevs: [1, 1]}
+  ],
+  parameters_repeated_measure_correlations: [
+    {table: [
+      [null, null, null, null],
+      [0.07, null, null, null],
+      [0,    0,    null, null],
+      [0,    0,    0,    null]
+    ]},
+    {table: [
+      [null, null],
+      [0   , null]
+    ]},
+  ],
+  parameters_intra_class_correlation: [1],
+  parameters_gaussian_covariate_variance: null,
+  parameters_gaussian_covariate_correlation: null,
+  parameters_scale_factor_variance: [1],
+  power_method: null,
+  power_curve: null
+}
