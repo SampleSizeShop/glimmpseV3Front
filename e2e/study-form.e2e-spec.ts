dceeb1994eeeeac79d08d83eab2b69695b4dc319
Qian <Andeeli@users.noreply.github.com>
2018-10-17 16:47:34 -0400

    VER3-324 Homework 3

--- e2e/study-form.e2e-spec.ts
+++ e2e/study-form.e2e-spec.ts
@@ -2,6 +2,8 @@ import {StudyFormComponentPage} from '../src/app/study-form/study-form.po';
 import {example_1_input, example_1_output} from './test_inputs/example_1_input';
 import {generic_model} from './test_inputs/generic_test_data_model';
 import {O2R2P2C0_input, O2R2P2C0_output} from './test_inputs/O2R2P2C0_output';
+import {hw2_input, hw2_output} from './test_inputs/homework2_longitudinal';
+import {hw3_input, hw3_output} from './test_inputs/homework3';
 
 describe('Glimmpse v3 automated integration tests', () => {
   let page: StudyFormComponentPage;
@@ -12,7 +14,7 @@ describe('Glimmpse v3 automated integration tests', () => {
     page.navigateTo('/design/MODE');
   });
 
-  it('Should calculate a power of alpha', async function() {
+  it('O2R2P2C0, Should calculate a power of alpha', async function() {
     const expected = O2R2P2C0_output;
     let actual = null;
     await page.fromJSON(O2R2P2C0_input);
@@ -25,9 +27,47 @@ describe('Glimmpse v3 automated integration tests', () => {
       actual = JSON.parse(text);
     });
     for (let i of actual.model) {
-      expect(actual.model.i).toBeCloseTo(expected.model.i,  6);
+      expect(actual.model.i).toBeCloseTo(expected.model.i, 6);
     }
-    expect(actual.results[0].power).toBeCloseTo(expected.results[0].power, 3);
+    actual.results.forEach((result, i) => {
+      expect(result.power).toBeCloseTo(expected.results[i].power, 4);
+    });
+  });
+
+  it('HW2, Should return correct power', async function() {
+    const expected = hw2_output;
+    let actual = null;
+    await page.fromJSON(hw2_input);
+    // await page.modelText().then(text => {
+    //   console.log(text);
+    // });
+    await page.calculate();
+    await page.output().then(text => {
+      console.log(text);
+      actual = JSON.parse(text);
+    });
+    for (let i of actual.model) {
+      expect(actual.model.i).toBeCloseTo(expected.model.i, 6);
+    }
+    expect(actual.results[0].power).toBeCloseTo(expected.results[0].power, 5);
+  });
+
+  it('HW3, Should return correct power', async function() {
+    const expected = hw3_output;
+    let actual = null;
+    await page.fromJSON(hw3_input);
+    // await page.modelText().then(text => {
+    //   console.log(text);
+    // });
+    await page.calculate();
+    await page.output().then(text => {
+      console.log(text);
+      actual = JSON.parse(text);
+    });
+    for (let i of actual.model) {
+      expect(actual.model.i).toBeCloseTo(expected.model.i, 6);
+    }
+    expect(actual.results[0].power).toBeCloseTo(expected.results[0].power, 5);
   });
 
   it('Should calculate a power of 1 for Grand Mean with only one outcome', async function() {
@@ -42,8 +82,8 @@ describe('Glimmpse v3 automated integration tests', () => {
     expect(actual.model).toEqual(expected.model);
     expect(actual.results[0].power).toEqual(expected.results[0].power);
   });
-  //
-  // it('Should fill out the study form: Nonsense example designed to hit every page in glimmpse v3 front end', () => {
-  //   page.fromJSON(generic_model);
-  // });
+
+  it('Should fill out the study form: Nonsense example designed to hit every page in glimmpse v3 front end', () => {
+    page.fromJSON(generic_model);
+  });
 });
