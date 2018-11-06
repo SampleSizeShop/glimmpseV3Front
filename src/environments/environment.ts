376c524d25aebd26bc4979e615cf50482c8ae43d
alasdair-macleod <alasdair.macleod@ufl.edu>
2018-11-06 09:22:34 -0500

    VER3-435 add class="col" to ensure paragraph and outcome table line up with headers and buttons

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   production: false,
   name: 'dev',
   serverLoggingUrl: 'http://127.0.0.1:5000/api/clientsidelog',
-  calculateUrl: 'http://127.0.0.1:5000/api/calculate',
+  calculateUrl: 'http://localhost:5000/api/calculate',
   loglevel: NgxLoggerLevel.DEBUG,
   serverLoglevel: NgxLoggerLevel.WARN,
   disableAnimations: true
