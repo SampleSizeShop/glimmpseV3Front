3ab73368a800949469213718c9dca37de4ae9860
alasdair-macleod <alasdair.macleod@ufl.edu>
2019-01-14 09:41:26 -0500

    VER3-467 fix typo

--- src/app/study-form/type-one-error/type-one-error.validator.ts
+++ src/app/study-form/type-one-error/type-one-error.validator.ts
@@ -4,7 +4,8 @@ export function typeOneErrorValidator(alphas: number[]): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } => {
 
     if (alphas.length < 1) {
-      return {'noalpha': 'You must specify at least one Type I Error rate.'};
+      return {'noalpha': 'You must specify at least one Type I Error rate.'}
+    };
 
     return null;
   }
