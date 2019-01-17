09331c53c77442e79611c6c617ddd3290c711de3
alasdair-macleod <alasdair.macleod@ufl.edu>
2019-01-17 15:06:25 -0500

    VER3-443 fix prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,12 @@
+import {NgxLoggerLevel} from 'ngx-logger';
+
 export const environment = {
-  production: true,
-  name: 'prod',
-  serverLoggingUrl: 'http://127.0.0.1:5000/api/clientsidelog',
-  loglevel: 'ERROR'
+  production: false,
+  name: 'dev',
+  serverLoggingUrl: 'http://54.161.9.34:5000/api/clientsidelog',
+  calculateUrl: 'http://54.161.9.34:5000/api/calculate',
+  loglevel: NgxLoggerLevel.DEBUG,
+  serverLoglevel: NgxLoggerLevel.WARN,
+  disableAnimations: false,
+  e2eTest: false
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import {NgxLoggerLevel} from 'ngx-logger';
 export const environment = {
   production: false,
   name: 'dev',
-  serverLoggingUrl: 'http://127.0.0.1:5000/api/clientsidelog',
+  serverLoggingUrl: 'http://54.161.9.34:5000/api/clientsidelog',
   calculateUrl: 'http://54.161.9.34:5000/api/calculate',
   loglevel: NgxLoggerLevel.DEBUG,
   serverLoglevel: NgxLoggerLevel.WARN,
