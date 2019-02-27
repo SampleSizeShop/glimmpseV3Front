aa90f7233357cc5b349e6cb4726b1b5d6ebbf9e8
alasdair-macleod <alasdair.macleod@ufl.edu>
2019-02-27 13:48:27 -0500

    VER3-487 fix environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   production: false,
   name: 'dev',
   serverLoggingUrl: 'http://54.161.9.34:5000/api/clientsidelog',
-  calculateUrl: 'http://127.0.0.1:5000/api/calculate',
+  calculateUrl: 'http://54.161.9.34:5000/api/calculate',
   loglevel: NgxLoggerLevel.DEBUG,
   serverLoglevel: NgxLoggerLevel.WARN,
   disableAnimations: false,
