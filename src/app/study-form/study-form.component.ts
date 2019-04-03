0b6ed9aebd5484d9b8f0dafe9fbff65c49422077
alasdair-macleod <alasdair.macleod@ufl.edu>
2019-04-03 16:28:50 -0400

    VER3-473 debug build

--- src/app/study-form/study-form.component.ts
+++ src/app/study-form/study-form.component.ts
@@ -656,9 +656,8 @@ export class StudyFormComponent implements OnInit, OnDestroy, DoCheck {
 
     this.quantilesSubscription = this.study_service.quantiles$.subscribe(
       quantiles => {
-        console.log(quantiles)
         if (!isNullOrUndefined(quantiles)) {
-          this.study.quantiles = Array.from(quantiles.values());
+          this.study.quantiles = quantiles;
         }
       }
     );
