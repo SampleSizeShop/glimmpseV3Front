497b00833bc9aad70440056ba9928db2b9dcccd6
alasdair-macleod <alasdair.macleod@ufl.edu>
2019-09-23 17:32:26 -0400

    VER3-272 fix typo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent {
 
   onFileChange(event) {
     const reader = new FileReader();
-    if(event.target.files && event.target.files.length > 0) {
+    if (event.target.files && event.target.files.length > 0) {
       const file = event.target.files[0];
       reader.readAsDataURL(file);
       reader.onload = () => {
