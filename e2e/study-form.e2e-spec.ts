515a049ffd802795234b93f2abddfa9f079b32ce
liqian <andeeleo@gmail.com>
2019-10-15 16:16:02 -0400

    VER3-553 added the E2E test of example 7

--- e2e/study-form.e2e-spec.ts
+++ e2e/study-form.e2e-spec.ts
@@ -30,6 +30,7 @@ import {powerlib_example3_input, powerlib_example3_output} from './test_inputs/P
 import {powerlib_example4_input, powerlib_example4_output} from './test_inputs/POWERLIB_Example_4';
 import {powerlib_example5_input, powerlib_example5_output} from './test_inputs/POWERLIB_Example_5';
 import {powerlib_example6_input, powerlib_example6_output} from './test_inputs/POWERLIB_Example_6';
+import {powerlib_example7_input, powerlib_example7_output} from './test_inputs/POWERLIB_Example_7';
 
 describe('Glimmpse v3 automated integration tests', () => {
   let page: StudyFormComponentPage;
@@ -235,7 +236,7 @@ describe('Glimmpse v3 automated integration tests', () => {
     expect(actual.results[0].power).toBeCloseTo(expected.results[0].power, 5);
   });
 
-  it('Should return the correct value in the detail table.', async function() {
+  it('detail_table, should return the correct value in the detail table.', async function() {
     const expected = com_calculate_detail_output;
     let groupCombination = null;
     let totalSampleSize = null;
@@ -460,7 +461,7 @@ describe('Glimmpse v3 automated integration tests', () => {
     });
   });
 
-  it('Should return the correct power', async function() {
+  it('Example1, should return the correct power', async function() {
     const expected = powerlib_example1_output;
     let actual = null;
     await page.fromJSON(powerlib_example1_input);
@@ -494,7 +495,7 @@ describe('Glimmpse v3 automated integration tests', () => {
       });
     });
 
-  it('Should return the correct power', async function() {
+  it('Example2, should return the correct power', async function() {
     const expected = powerlib_example2_output;
     let actual = null;
     await page.fromJSON(powerlib_example2_input);
@@ -528,7 +529,7 @@ describe('Glimmpse v3 automated integration tests', () => {
     });
   });
 
-  it('Should return the correct power', async function() {
+  it('Example3, should return the correct power', async function() {
     const expected = powerlib_example3_output;
     let actual = null;
     await page.fromJSON(powerlib_example3_input);
@@ -562,7 +563,7 @@ describe('Glimmpse v3 automated integration tests', () => {
     });
   });
 
-  it('Should return the correct power', async function() {
+  it('Example4, should return the correct power', async function() {
     const expected = powerlib_example4_output;
     let actual = null;
     await page.fromJSON(powerlib_example4_input);
@@ -596,7 +597,7 @@ describe('Glimmpse v3 automated integration tests', () => {
     });
   });
 
-  it('Should return the correct power', async function() {
+  it('Example5, should return the correct power', async function() {
     const expected = powerlib_example5_output;
     let actual = null;
     await page.fromJSON(powerlib_example5_input);
@@ -630,7 +631,7 @@ describe('Glimmpse v3 automated integration tests', () => {
     });
   });
 
-  it('Should return the correct power', async function() {
+  it('Example6, should return the correct power', async function() {
     const expected = powerlib_example6_output;
     let actual = null;
     await page.fromJSON(powerlib_example6_input);
@@ -662,6 +663,42 @@ describe('Glimmpse v3 automated integration tests', () => {
         expect(r.power).toBeCloseTo(e.power, 5);
         expect(r.lower_bound).toBeCloseTo(e.lower_bound, 4);
         expect(r.upper_bound).toBeCloseTo(e.upper_bound, 5);
+      }
+    });
+  });
+
+  it('Example7, should return the correct power', async function() {
+    const expected = powerlib_example7_output;
+    let actual = null;
+    await page.fromJSON(powerlib_example7_input);
+    await page.calculate();
+    await page.output().then(text => {
+      console.log(text);
+      actual = JSON.parse(text);
+    });
+
+    actual.results.forEach((result) => {
+      let match = false;
+      let r = null;
+      let e = null;
+      expected.results.forEach(exp => {
+        if (
+          exp.alpha === result.model.alpha
+          && exp.mean_scale_factor === result.model.means_scale_factor
+          && exp.variability_scale_factor === result.model.variance_scale_factor
+          && exp.test === result.test
+          && exp.total_N === result.model.total_n) {
+          match = true;
+          r = result;
+          e = exp;
+        }
+      });
+      if (!match) {
+        expect(false).toBe(true);
+      } else {
+        expect(r.power).toBeCloseTo(e.power, 4);
+        expect(r.lower_bound).toBeCloseTo(e.lower_bound, 4);
+        expect(r.upper_bound).toBeCloseTo(e.upper_bound, 4);
       }
     });
   });
