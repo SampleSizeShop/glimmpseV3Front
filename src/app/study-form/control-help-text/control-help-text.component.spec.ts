98495e4733cc64ad62220cc1c690aad117698187
alasdair-macleod <alasdair.macleod@ufl.edu>
2019-10-18 14:29:44 -0400

    VER3-272 add control help text component and replace tutorial link with this.

---
+++ src/app/study-form/control-help-text/control-help-text.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ControlHelpTextComponent } from './control-help-text.component';
+
+describe('ControlHelpTextComponent', () => {
+  let component: ControlHelpTextComponent;
+  let fixture: ComponentFixture<ControlHelpTextComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ControlHelpTextComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ControlHelpTextComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/study-form/control-help-text/control-help-text.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-control-help-text',
+  templateUrl: './control-help-text.component.html',
+  styleUrls: ['./control-help-text.component.scss']
+})
+export class ControlHelpTextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
