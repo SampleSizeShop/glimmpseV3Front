c4303059956659ba8af788f9b383b848a422fa18
alasdair-macleod <alasdair.macleod@ufl.edu>
2019-11-06 15:32:16 -0500

    VER3-560 move power form to TargetPowerComponent

--- src/app/study-form/target-power/target-power.component.ts
+++ src/app/study-form/target-power/target-power.component.ts
@@ -1,15 +1,264 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, DoCheck, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Observable, Subscription} from 'rxjs/index';
+import {minMaxValidator} from '../../shared/validators/minmax.validator';
+import {constants} from '../../shared/model/constants';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {NavigationService} from '../../shared/services/navigation.service';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {NGXLogger} from 'ngx-logger';
+import {StudyService} from '../../shared/services/study.service';
+import {of as observableOf, Subscription, Observable} from 'rxjs';
+import {minMaxValidator} from '../../shared/validators/minmax.validator';
+import {FormBuilder, FormGroup} from '@angular/forms';
+
 
 @Component({
   selector: 'app-target-power',
   templateUrl: './target-power.component.html',
   styleUrls: ['./target-power.component.scss']
 })
-export class TargetPowerComponent implements OnInit {
+export class TargetPowerComponent implements OnInit, DoCheck, OnDestroy {
 
-  constructor() { }
+  private _powerSubscription: Subscription;
+  private _navigationSubscription: Subscription;
+  private _showHelpTextSubscription: Subscription;
+  private _powerSampleSizeForm: FormGroup;
+  private _power: number[];
+  private _formErrors = constants.TARGET_EVENT_FORM_ERRORS;
+  private _validationMessages = constants.TARGET_EVENT_VALIDATION_MESSAGES;
+
+  @ViewChild('helpText', {static: true}) helpTextModal;
+  private helpTextModalReference: any;
+  private _afterInit: boolean;
+
+  constructor(private study_service: StudyService,
+              private fb: FormBuilder,
+              private log: NGXLogger,
+              private _navigation_service: NavigationService,
+              private modalService: NgbModal) {
+
+    this.powerSubscription = this.study_service.power$.subscribe(
+      power => {
+        this.power = power;
+      }
+    );
+    this._navigationSubscription = this.study_service.navigationDirection$.subscribe(
+      direction => {
+        this._directionCommand = direction;
+        this.checkValidBeforeNavigation(this._directionCommand);
+      }
+    );
+    this._showHelpTextSubscription = this._navigation_service.helpText$.subscribe( help => {
+      if (this._afterInit) {
+        this.showHelpText(this.helpTextModal);
+      }
+    });
+    this.buildForm();
+  }
 
   ngOnInit() {
+    this._afterInit = true;
+    if (
+      this._power === null || this._power === undefined || this._power.length === 0
+    ) {
+      this.setNextEnabled('INVALID');
+    }
+  }
+
+  ngDoCheck() {
+    this.study_service.updatePower(this.power);
+    this.checkValidBeforeNavigation('NEXT')
+  }
+
+  ngOnDestroy() {
+    this.setNextEnabled('VALID');
+    this._powerSubscription.unsubscribe();
+    this._navigationSubscription.unsubscribe();
+    this._showHelpTextSubscription.unsubscribe();
+  }
+
+  buildForm(): void {
+    let powerDefault = 0.9;
+    if (
+      this.power !== null
+      && this.power !== undefined
+      && this.power.length > 0
+    ) {
+      powerDefault = null;
+    }
+    this.powerSampleSizeForm = this.fb.group({
+      power: [powerDefault, minMaxValidator(0, 1, this.log)],
+      ciwidth: [this.ciwidth, minMaxValidator(0, 10, this.log)]
+    });
+
+    this.powerSampleSizeForm.valueChanges.subscribe(data => this.onValueChanged(data));
+    this.onValueChanged(); // (re)set validation messages now
+  }
+
+  addPower() {
+    const value = this.powerSampleSizeForm.value.power;
+    if (
+      value !== null
+      && value !== undefined
+      && value !== ''
+      && this._power.indexOf(value) === -1
+      && !this.formErrors.power
+    ) {
+      this._power.push(value);
+      this.powerSampleSizeForm.reset();
+    }
+  }
+
+  removePower(value: number) {
+    const index = this.power.indexOf(value);
+    if (index > -1) {
+      this.power.splice(index, 1);
+    }
+    this.powerSampleSizeForm.reset();
+  }
+
+  firstPower(): boolean {
+    return this.power.length === 0 ? true : false;
+  }
+
+  get powers$(): Observable<number[]> {
+    return observableOf(this.power)
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.powerSampleSizeForm) {
+      return;
+    }
+    const form = this.powerSampleSizeForm;
+
+    for (const field of Object.keys(this.formErrors)) {
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key of Object.keys(control.errors)) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  hasError() {
+    if (this._formErrors.power === null || this._formErrors.power === undefined) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  rowStyle(index: number) {
+    if (index % 2 === 1) {
+      return 'col col-md-auto table-active';
+    } else {
+      return 'col col-md-auto table-primary';
+    }
+  }
+
+  checkValidBeforeNavigation(direction: string): void {
+    this.checkValidator();
+    if ( direction === 'NEXT' ) {
+      if (
+        this._power === null || this._power === undefined || this._power.length === 0
+      ) {
+        this.setNextEnabled('INVALID');
+      } else if (this._power && !this.formErrors.power) {
+        this.setNextEnabled('VALID');
+      }
+    }
+  }
+
+  checkValidator(data?: any) {
+    if (!this.powerSampleSizeForm) {
+      return;
+    }
+    const form = this.powerSampleSizeForm;
+
+    for (const field of Object.keys(this.validationMessages)) {
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key of Object.keys(control.errors)) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  setNextEnabled(status: string) {
+    const valid = status === 'VALID' ? true : false;
+    this._navigation_service.updateValid(valid);
+  }
+
+  dismissHelp() {
+    this.helpTextModalReference.close();
+  }
+
+  showHelpText(content) {
+    this.modalService.dismissAll();
+    this.helpTextModalReference = this.modalService.open(content);
+    this.helpTextModalReference.result.then(
+      (closeResult) => {
+        this.log.debug('modal closed : ' + closeResult);
+      }, (dismissReason) => {
+        if (dismissReason === ModalDismissReasons.ESC) {
+          this.log.debug('modal dismissed when used pressed ESC button');
+        } else if (dismissReason === ModalDismissReasons.BACKDROP_CLICK) {
+          this.log.debug('modal dismissed when used pressed backdrop');
+        } else {
+          this.log.debug(dismissReason);
+        }
+      });
+  }
+
+  get powerSubscription(): Subscription {
+    return this._powerSubscription;
+  }
+
+  set powerSubscription(value: Subscription) {
+    this._powerSubscription = value;
+  }
+
+  get formErrors(): { power: string; ciwidth: string } {
+    return this._formErrors;
+  }
+
+  set formErrors(value: { power: string; ciwidth: string }) {
+    this._formErrors = value;
+  }
+
+  get validationMessages(): {
+    power: { minval: string; maxval: string };
+  } {
+    return this._validationMessages;
+  }
+
+  set validationMessages(value: {
+    power: { minval: string; maxval: string };
+  }) {
+    this._validationMessages = value;
+  }
+
+  get power(): number[] {
+    return this._power;
+  }
+
+  set power(value: number[]) {
+    this._power = value;
+  }
+
+  get powerSampleSizeForm(): FormGroup {
+    return this._powerSampleSizeForm;
+  }
+
+  set powerSampleSizeForm(value: FormGroup) {
+    this._powerSampleSizeForm = value;
   }
 
 }
